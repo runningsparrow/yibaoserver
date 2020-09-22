940f1896dde422b9602a0191f76ab59db1c4d6d0
runningsparrow <runningsparrow@gmail.com>
2020-09-22 17:30:40 +0800

    0922

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css'
+import './style.less'  //引入less文件
 
 ReactDOM.render(
   // <React.StrictMode>
