4182e655012e82d21f0aca60ad044b4c79939179
runningsparrow <runningsparrow@gmail.com>
2021-01-28 23:46:53 +0800

    '0128-2'

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -6,7 +6,18 @@ export default class Header extends Component {
     render(){
         return(
             <div className="header">
-                Header
+                <div className="header-top">
+                    <span>欢迎, admin</span>
+                    <a href="javascript:">退出</a>
+                </div>
+                <div className="header-bottom">
+                    <div className="header-bottom-left">首页</div>
+                    <div className="header-bottom-right">
+                        <span>2021-01-28 22:22:22</span>
+                        <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="weather"></img>
+                        <span>晴</span>
+                    </div>
+                </div>
             </div>
         )
     }
